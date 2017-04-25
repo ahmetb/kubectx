a861562803af77115be8ab7347caccf82ac4d127
Ahmet Alp Balkan <ahmetb@google.com>
2017-04-25 13:21:42 -0700

    Formula polishing
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -1,24 +1,24 @@
 class Kubectx < Formula
-    desc "Tool that can switch between kubectl contexts easily and create aliases"
-    homepage "https://github.com/ahmetb/kubectx"
-    url "https://github.com/ahmetb/kubectx/archive/v0.1.zip"
-    sha256 "3d014027e38c476164638b2138f190c43fd65a22ec50035c36926555233247c0"
+  desc "Tool that can switch between kubectl contexts easily and create aliases"
+  homepage "https://github.com/ahmetb/kubectx"
+  url "https://github.com/ahmetb/kubectx/archive/v0.1.tar.gz"
+  sha256 "841817f928af25061b1b6794400394c3e6e807e8a1c48c179f1fd8bdd553ca79"
 
-    bottle :unneeded
+  bottle :unneeded
 
-    def install
-        bin.install "kubectx"
-        bash_completion.install "completion/kubectx.bash" => "kubectx"
-    end
+  def install
+    bin.install "kubectx"
+    bash_completion.install "completion/kubectx.bash" => "kubectx"
+  end
 
-    test do
-        system "kubectx", "--help"
-    end
+  def caveats; <<-EOS.undent
+    To install zsh completion, add this to your .zshrc:
 
-    def caveats; <<-EOS.undent
-      To install zsh completion, add this to your .zshrc:
+      [ -f /usr/local/etc/bash_completion.d/kubectx ] && source /usr/local/etc/bash_completion.d/kubectx
+    EOS
+  end
 
-        [ -f /usr/local/etc/bash_completion.d/kubectx ] && source /usr/local/etc/bash_completion.d/kubectx
-      EOS
-    end
+  test do
+    system "kubectx", "--help"
+  end
 end
