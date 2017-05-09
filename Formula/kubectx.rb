d44150534810b6c309be75363cf4d83d3f023e22
Ahmet Alp Balkan <ahmetb@google.com>
2017-05-09 16:23:22 -0700

    Add kubens tool for namespace switching
    Fixes #1. Now kubectx also ships with kubens.
    
    Extracted utility functions to a utils.bash file, loaded
    from ../include/utils.bash.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -3,16 +3,23 @@ class Kubectx < Formula
   homepage "https://github.com/ahmetb/kubectx"
   url "https://github.com/ahmetb/kubectx/archive/v0.2.0.tar.gz"
   sha256 "28069aff84aaba1aa38f42d3b27e64e460a5c0651fb56b1748f44fd832d912e3"
+  head "https://github.com/ahmetb/kubectx.git", :branch => "master"
+
 
   bottle :unneeded
 
   def install
     bin.install "kubectx"
+    bin.install "kubens"
+    include.install "utils.bash"
     bash_completion.install "completion/kubectx.bash" => "kubectx"
+    bash_completion.install "completion/kubens.bash" => "kubens"
     zsh_completion.install "completion/kubectx.zsh" => "_kubectx"
+    zsh_completion.install "completion/kubens.zsh" => "_kubens"
   end
 
   test do
     system "which", "kubectx"
+    system "which", "kubens"
   end
 end
