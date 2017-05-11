d04543ffef61e777f71baa9f49b79e2e91b95fd0
Simon Westcott <swestcott@gmail.com>
2017-05-11 12:44:11 +0100

    Fix context discovery

--- completion/kubectx.bash
+++ completion/kubectx.bash
@@ -2,7 +2,7 @@ _kube_contexts()
 {
   local curr_arg;
   curr_arg=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts | awk '{print $2}' | tail -n +2)" -- $curr_arg ) );
+  COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) );
 }
 
 complete -F _kube_contexts kubectx
