7e10232fdd806d667ef875bd4be7a0d8f58aafe8
Ahmet Alp Balkan <ahmetb@google.com>
2017-05-16 22:19:32 -0700

    Update Homebrew Formula with v0.3.0
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -1,8 +1,8 @@
 class Kubectx < Formula
   desc "Tool that can switch between kubectl contexts easily and create aliases"
   homepage "https://github.com/ahmetb/kubectx"
-  url "https://github.com/ahmetb/kubectx/archive/v0.2.0.tar.gz"
-  sha256 "28069aff84aaba1aa38f42d3b27e64e460a5c0651fb56b1748f44fd832d912e3"
+  url "https://github.com/ahmetb/kubectx/archive/v0.3.0.tar.gz"
+  sha256 "720b6d9a960a7583983ac84c90d2ff89f4494584cf82a3abb5d4e66e72444621"
   head "https://github.com/ahmetb/kubectx.git", :branch => "short-names"
   bottle :unneeded
 
