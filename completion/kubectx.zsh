5d79a0663dc2929d845e466099bc3cc073484140
Ahmet Alp Balkan <ahmetb@google.com>
2017-10-23 23:56:21 -0700

    Remove message for '-' in kubectx.zsh
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- completion/kubectx.zsh
+++ completion/kubectx.zsh
@@ -5,7 +5,7 @@ PREV=""
 if [ -f "$KUBECTX" ]; then
     # show '-' only if there's a saved previous context
     local PREV=$(cat "${KUBECTX}")
-    _arguments "1: :((-\:Back\ to\ ${PREV} \
+    _arguments "1: :((- \
         $(kubectl config get-contexts --output='name')))"
 else
     _arguments "1: :($(kubectl config get-contexts --output='name'))"
