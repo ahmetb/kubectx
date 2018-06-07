1316d3a6c8405b1b179d7f500a810e0628ed7179
Julian Vassev <jvassev@gmail.com>
2018-06-07 03:32:12 -0700

    Import kubeconfig using -i
    Add `-i` which import another kubeconfig at the default location.
    
    It will never overwrite an existing context with the same name but
    instead will rename the incoming context just before import.
    
    Example:
    ```bash
    $ kubectx
    foo
    bar
    
    $ kubectx -i /tmp/hello.yaml
    kubectx -i /tmp/hello
    Importing kubeconfig from /tmp/hello:
      + hello.yaml-c731b0dc21b4-bar
    
    /tmp/hello.yaml
    ```

--- completion/kubectx.bash
+++ completion/kubectx.bash
@@ -1,8 +1,14 @@
 _kube_contexts()
 {
-  local curr_arg;
+  local curr_arg
   curr_arg=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) );
+  if [[ "${COMP_WORDS[$(($COMP_CWORD - 1))]}" == "-i" ]]; then
+    compopt -o default
+    COMPREPLY=()
+  else
+    compopt +o default
+    COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) )
+  fi
 }
 
 complete -F _kube_contexts kubectx kctx
