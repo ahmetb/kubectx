3f8009680d7f28dd28e5a68c7149d12030e6bec3
Dror Levy <Dror@tablecheck.com>
2020-04-17 21:36:31 +0900

    feat(completion): Improve kubens fish completions This change covers all of the supported arguments and switches for the kubens command

--- completion/kubens.fish
+++ completion/kubens.fish
@@ -1,3 +1,12 @@
 # kubens
-complete -f -c kubens -a "(kubectl get ns -o=custom-columns=NAME:.metadata.name --no-headers)"
 
+function __fish_kubens_arg_number -a number
+    set -l cmd (commandline -opc)
+    test (count $cmd) -eq $number
+end
+
+complete -f -c kubens
+complete -f -x -c kubens -n '__fish_kubens_arg_number 1' -a "(kubectl get ns -o=custom-columns=NAME:.metadata.name --no-headers)"
+complete -f -x -c kubens -n '__fish_kubens_arg_number 1' -a "-" -d "switch to the previous namespace in this context"
+complete -f -x -c kubens -n '__fish_kubens_arg_number 1' -s c -l current -d "show the current namespace"
+complete -f -x -c kubens -n '__fish_kubens_arg_number 1' -s h -l help -d "show the help message"
