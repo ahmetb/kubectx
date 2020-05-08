69c1a3b518cc2f713ec0d410a287cef35d72dcb2
sedat <sedat@mobilabsolutions.com>
2020-05-08 03:37:59 +0200

    Apply gofmt.

--- internal/kubeconfig/helper_test.go
+++ internal/kubeconfig/helper_test.go
@@ -11,12 +11,12 @@ type MockKubeconfigLoader struct {
 	out bytes.Buffer
 }
 
-func (t *MockKubeconfigLoader) Read(p []byte) (n int, err error)    { return t.in.Read(p) }
-func (t *MockKubeconfigLoader) Write(p []byte) (n int, err error)   { return t.out.Write(p) }
-func (t *MockKubeconfigLoader) Close() error                        { return nil }
-func (t *MockKubeconfigLoader) Reset() error                        { return nil }
+func (t *MockKubeconfigLoader) Read(p []byte) (n int, err error)          { return t.in.Read(p) }
+func (t *MockKubeconfigLoader) Write(p []byte) (n int, err error)         { return t.out.Write(p) }
+func (t *MockKubeconfigLoader) Close() error                              { return nil }
+func (t *MockKubeconfigLoader) Reset() error                              { return nil }
 func (t *MockKubeconfigLoader) Load(string) (ReadWriteResetCloser, error) { return t, nil }
-func (t *MockKubeconfigLoader) Output() string                      { return t.out.String() }
+func (t *MockKubeconfigLoader) Output() string                            { return t.out.String() }
 
 func WithMockKubeconfigLoader(kubecfg string) *MockKubeconfigLoader {
 	return &MockKubeconfigLoader{in: strings.NewReader(kubecfg)}
