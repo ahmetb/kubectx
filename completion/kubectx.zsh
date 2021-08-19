e449e739f8909e73b16e51d50fbe1054ed9ccd2a
Johan Dewe <johan@dewe.net>
2021-08-19 19:04:48 +0200

    Wrap context names in single quotes to prevent completion script to fail (#316)

--- completion/kubectx.zsh
+++ completion/kubectx.zsh
@@ -3,7 +3,9 @@
 local KUBECTX="${HOME}/.kube/kubectx"
 PREV=""
 
-local all_contexts="$(kubectl config get-contexts --output='name')"
+local context_array=("${(@f)$(kubectl config get-contexts --output='name')}")
+local all_contexts=(\'${^context_array}\')
+
 if [ -f "$KUBECTX" ]; then
     # show '-' only if there's a saved previous context
     local PREV=$(cat "${KUBECTX}")
